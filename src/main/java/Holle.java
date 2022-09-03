d21fddbe4b508ebebd97cf5c0e659782d1f5534a
chenjy <3221347421@qq.com>
2022-09-03 17:42:45 +0800

    陈靖宇测试2

--- src/main/java/Holle.java
+++ src/main/java/Holle.java
@@ -1,7 +1,7 @@
 public class Holle {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("陈靖宇神采奕奕");
+        System.out.println("陈靖宇到此一游");
         System.out.println("盛振宇到此一游");
     }
 
